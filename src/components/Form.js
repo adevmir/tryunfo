30ac3b7f6f198f3183cbbb06e36ca792ff625bde
adevmir <ademirvalonqar@gmail.com>
2022-04-12 11:55:26 -0300

    [Requisito 1]

---
+++ src/components/Form.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form className="form">
+
+          <label htmlFor="text">
+            Nome:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              data-testid="name-input"
+            />
+          </label>
+
+          <label htmlFor="desc">
+            Descrição da carta:
+            <input
+              id="desc"
+              name="desc"
+              type="textarea"
+              data-testid="name-input"
+            />
+          </label>
+
+          <label htmlFor="forc">
+            Força:
+            <input
+              id="forc"
+              name="forc"
+              type="number"
+              data-testid="attr1-input"
+            />
+          </label>
+
+          <label htmlFor="defe">
+            Defesa:
+            <input
+              id="defe"
+              name="defe"
+              type="number"
+              data-testid="attr2-input"
+            />
+          </label>
+
+          <label htmlFor="velo">
+            Velocidade:
+            <input
+              id="velo"
+              name="velo"
+              type="number"
+              data-testid="attr3-input"
+            />
+          </label>
+
+          <label htmlFor="img">
+            Imagem da carta:
+            <input
+              id="img"
+              name="img"
+              data-testid="image-input"
+            />
+          </label>
+
+          <label htmlFor="rari">
+            Raridade:
+            <select
+              id="rari"
+              name="rari"
+              data-testid="rare-input"
+            >
+              <option value="normal"> Normal </option>
+              <option value="raro"> Raro </option>
+              <option value="muito raro"> Muito Raro </option>
+            </select>
+          </label>
+
+          <label htmlFor="stry">
+            <input
+              id="stry"
+              name="stry"
+              type="checkbox"
+            />
+            Carta Super Tryunfo?
+          </label>
+
+          <button
+            id="button"
+            type="button"
+            data-testid="save-button"
+          >
+            Salvar
+          </button>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
